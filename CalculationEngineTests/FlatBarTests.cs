0934983ba4eedcdb8ece34864d35feceb961fa39
Billy Noble <billy.noble.2022@uni.strath.ac.uk>
2024-07-31 22:00:55 +0100

    Unit test reworked.

--- CalculationEngineTests/FlatBarTests.cs
+++ CalculationEngineTests/FlatBarTests.cs
@@ -1,4 +1,5 @@
 using StructuralAPI.CalculationEngines;
+using StructuralAPI.Models;
 
 namespace CalculationEngineTests
 {
@@ -7,15 +8,22 @@ namespace CalculationEngineTests
         [Test]
         public void Test1()
         {
-
-            //SHOULD SPOOF A REQUEST
             //Arrange
-            double width = 200;
-            double thickness = 20;
+            FlatBarDataIn flatBar = new FlatBarDataIn();
+            flatBar.TensionLoad = 200;
+            flatBar.Width = 200;
+            flatBar.Thickness = 20;
+            flatBar.BoltDiameter = 20;
+            flatBar.PlateGrade = "355";
+            flatBar.BoltGrade = 8.8;
+
 
             //Act
-            double actualResult = StructuralAPI.CalculationEngines.FlatBarDesigner.CalculateGrossArea(width, thickness);
-            double expectedResult = 4000;
+            FlatBarDataOut actualResult = StructuralAPI.CalculationEngines.FlatBarDesigner.Calculator(flatBar);
+
+            FlatBarDataOut expectedResult = new FlatBarDataOut{
+                
+            };
 
             //Assert
             Assert.AreEqual(actualResult, expectedResult);
