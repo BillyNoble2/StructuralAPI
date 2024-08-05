36d5891c0db93fa035b02d8dd3f586d6c2afff32
Billy Noble <billy.noble.2022@uni.strath.ac.uk>
2024-08-05 19:34:26 +0100

    Added full unit test.

--- CalculationEngineTests/FlatBarTests.cs
+++ CalculationEngineTests/FlatBarTests.cs
@@ -21,12 +21,37 @@ namespace CalculationEngineTests
             //Act
             FlatBarDataOut actualResult = StructuralAPI.CalculationEngines.FlatBarDesigner.Calculator(flatBar);
 
-            FlatBarDataOut expectedResult = new FlatBarDataOut{
-                
+            FlatBarDataOut expectedResult = new FlatBarDataOut
+            {
+                GrossArea = 4000,
+                BoltHoleDiameter = 22,
+                NumberOfBolts = 3,
+                BoltsPerRow = 3,
+                NetArea = 2680,
+                TotalBoltShearRes = 282.3,
+                PlateFu = 490,
+                TensionResistance = 1074.4,
+                DimA = 30,
+                DimB = 70,
+                DimC = 40,
+                DimD = 60,
+                Diagram = "B"
             };
 
             //Assert
-            Assert.AreEqual(actualResult, expectedResult);
+            Assert.AreEqual(expectedResult.GrossArea, actualResult.GrossArea);
+            Assert.AreEqual(expectedResult.BoltHoleDiameter, actualResult.BoltHoleDiameter);
+            Assert.AreEqual(expectedResult.NumberOfBolts, actualResult.NumberOfBolts);
+            Assert.AreEqual(expectedResult.BoltsPerRow, actualResult.BoltsPerRow);
+            Assert.AreEqual(expectedResult.NetArea, actualResult.NetArea);
+            Assert.AreEqual(expectedResult.TotalBoltShearRes, actualResult.TotalBoltShearRes);
+            Assert.AreEqual(expectedResult.PlateFu, actualResult.PlateFu);
+            Assert.AreEqual(expectedResult.TensionResistance, actualResult.TensionResistance);
+            Assert.AreEqual(expectedResult.DimA, actualResult.DimA);
+            Assert.AreEqual(expectedResult.DimB, actualResult.DimB);
+            Assert.AreEqual(expectedResult.DimC, actualResult.DimC);
+            Assert.AreEqual(expectedResult.DimD, actualResult.DimD);
+            Assert.AreEqual(expectedResult.Diagram, actualResult.Diagram);
         }
     }
 }
