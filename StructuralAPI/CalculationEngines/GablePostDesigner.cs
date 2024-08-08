5542114b06e175a65209f05c9e7be3a2bc0f50cf
Billy Noble <billy.noble.2022@uni.strath.ac.uk>
2024-08-08 12:38:56 +0100

    Started work on gable post designer.

---
+++ StructuralAPI/CalculationEngines/GablePostDesigner.cs
@@ -0,0 +1,65 @@
+﻿using StructuralAPI.Models;
+
+namespace StructuralAPI.CalculationEngines
+{
+    public class GablePostDesigner
+    {
+        public static void GablePostCalculator(GablePostDataIn request)
+        {
+            double windUDL = CalculateUniformWindLoad(request.PositiveWindPressure, request.ColumnCentres, request.WindLoadFactor);
+            double factoredAxialLoad = CalculateAxialLoad(request.DeadLoadFactor, request.LiveLoadFactor, request.RoofDeadLoad, request.RoofLiveLoad, request.ColumnCentres, request.FrameCentres);
+            double factoredBendingMoment = CalculateBendingMoment(windUDL, request.WindLoadFactor, request.ColumnHeight);
+        }
+        /// <summary>
+        /// Calculate uniform wind load on post (Factored).
+        /// </summary>
+        /// <param name="posWindPress"></param>
+        /// <param name="columnHeight"></param>
+        /// <param name="columnCentres"></param>
+        /// <returns>UDL (Factored)</returns>
+        public static double CalculateUniformWindLoad(double posWindPress, double columnCentres, double wLFactor)
+        {
+            return (posWindPress * columnCentres);
+        }
+        /// <summary>
+        /// Calculate axial load in post (Factored)
+        /// </summary>
+        /// <param name="dLFactor"></param>
+        /// <param name="lLFactor"></param>
+        /// <param name="roofDeadLoad"></param>
+        /// <param name="roofLiveLoad"></param>
+        /// <param name="columnCentres"></param>
+        /// <param name="frameCentres"></param>
+        /// <returns></returns>
+        public static double CalculateAxialLoad(double dLFactor, double lLFactor, double roofDeadLoad, double roofLiveLoad, double columnCentres, double frameCentres)
+        {
+            var halfFrameCentre = frameCentres / 2;
+            var roofArea = halfFrameCentre * columnCentres;
+            var factoredAreaLoad = (dLFactor * roofDeadLoad) + (lLFactor * roofLiveLoad);
+
+            return (factoredAreaLoad * roofArea);
+        }
+        /// <summary>
+        /// Calculates bending moment due to wind load (Factored)
+        /// </summary>
+        /// <param name="windUDL"></param>
+        /// <param name="wLFactor"></param>
+        /// <param name="columnHeight"></param>
+        /// <returns>Factored bending moment</returns>
+        public static double CalculateBendingMoment(double windUDL, double wLFactor, double columnHeight)
+        {
+            return ((windUDL * (columnHeight*columnHeight))/8)*wLFactor;
+        }
+        /// <summary>
+        /// Calculates shear force due to wind load (Factored)
+        /// </summary>
+        /// <param name="windUDL"></param>
+        /// <param name="wLFactor"></param>
+        /// <param name="columnHeight"></param>
+        /// <returns>Factored shear force</returns>
+        public static double CalculateShearForce(double windUDL, double wLFactor, double columnHeight)
+        {
+            return ((windUDL * columnHeight) / 2) * windUDL;
+        }
+    }
+}
---
+++ StructuralAPI/Models/GablePostDataIn.cs
@@ -0,0 +1,16 @@
+﻿namespace StructuralAPI.Models
+{
+    public class GablePostDataIn
+    {
+        public double ColumnHeight { get; set; }
+        public double ColumnCentres { get; set; }
+        public double FrameCentres { get; set; }
+        public double PositiveWindPressure { get; set; }
+        public double DeadLoadFactor { get; set; }
+        public double LiveLoadFactor { get; set; }
+        public double WindLoadFactor { get; set; }
+        public double RoofDeadLoad {  get; set; }
+        public double RoofLiveLoad { get; set; }
+
+    }
+}
